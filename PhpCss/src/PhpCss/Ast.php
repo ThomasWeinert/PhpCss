8013a746bf17a378d832adf014818693e2427871
Thomas Weinert <thomas@weinert.info>
2010-05-29 16:32:46 +0000

    - Documented: PhpCssAst

--- PhpCss/src/PhpCss/Ast.php
+++ PhpCss/src/PhpCss/Ast.php
@@ -1,7 +1,27 @@
 <?php
+/**
+* Abstract superclass of all elements in the abstract syntax tree.
+*
+* @version $Id$
+* @license http://www.opensource.org/licenses/mit-license.php The MIT License
+* @copyright Copyright 2010 PhpCss Team
+*
+* @package PhpCss
+* @subpackage Ast
+*/
 
+/**
+* Abstract superclass of all elements in the abstract syntax tree.
+* @package PhpCss
+* @subpackage Ast
+*/
 abstract class PhpCssAst {
   
+  /**
+  * The visitors are used to extract information from an ast.
+  * 
+  * @param PhpCssAstVisitor $visitor
+  */
   abstract public function visit(PhpCssAstVisitor $visitor);
   
 }
