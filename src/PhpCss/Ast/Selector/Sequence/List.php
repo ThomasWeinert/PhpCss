f8e865243b0ffab7ad1fc65138f885d7389371f4
Thomas Weinert <thomas@weinert.info>
2012-06-07 15:46:35 +0200

    Added Sequence List Ast node class, will be the root of a parsed selector string

---
+++ src/PhpCss/Ast/Selector/Sequence/List.php
@@ -0,0 +1,119 @@
+<?php
+/**
+* List of Css Selector Sequences.
+*
+* @version $Id$
+* @license http://www.opensource.org/licenses/mit-license.php The MIT License
+* @copyright Copyright 2010 PhpCss Team
+*
+* @package PhpCss
+* @subpackage Ast
+*/
+
+/**
+* List of Css Selector Sequences.
+*
+* This is the root element of a standard selector string like:
+* "element, .class"
+*
+* Because it is a list the some standard interfaces are implemented for
+* easier usage.
+*
+* @package PhpCss
+* @subpackage Ast
+*/
+class PhpCssAstSelectorSequenceList
+  extends PhpCssAstSelector
+  implements ArrayAccess, Countable, IteratorAggregate {
+
+  private $_sequences = array();
+
+  /**
+  * Create the object and assign sequences if provided. They
+  * can be added later of course.
+  *
+  * @param array $sequences
+  */
+  public function __construct(array $sequences = array()) {
+    foreach ($sequences as $sequence) {
+      $this->offsetSet(NULL, $sequence);
+    }
+  }
+
+  /**
+  * Check if a sequence at the given position is available in the list.
+  *
+  * @see ArrayAccess::offsetExists()
+  * @param integer $offset
+  * @return boolean
+  */
+  public function offsetExists($offset) {
+    return isset($this->_sequences[$offset]);
+  }
+
+  /**
+  * Return the sequence at the given position.
+  *
+  * @see ArrayAccess::offsetGet()
+  * @param integer $offset
+  * @return PhpCssAstSelectorSequence
+  */
+  public function offsetGet($offset) {
+    return $this->_sequences[$offset];
+  }
+
+  /**
+  * Set/Add and sequence at the given position or top the end
+  *
+  * @see ArrayAccess::offsetSet()
+  * @param integer|NULL $offset
+  * @param PhpCssAstSelectorSequence
+  */
+  public function offsetSet($offset, $sequence) {
+    if (!$sequence instanceOf PhpCssAstSelectorSequence) {
+      throw new InvalidArgumentException(
+        sprintf(
+          '$sequence is not an instance of PhpCssAstSelectorSequence but %s.',
+          is_object($sequence) ? get_class($sequence) : gettype($sequence)
+        )
+      );
+    }
+    if (is_null($offset)) {
+      $this->_sequences[] = $sequence;
+    } else {
+      $this->_sequences[(int)$offset] = $sequence;
+      $this->_sequences = array_values($this->_sequences);
+    }
+  }
+
+  /**
+  * Remove the sequence at the given position
+  *
+  * @see ArrayAccess::offsetUnset()
+  * @param integer $offset
+  */
+  public function offsetUnset($offset) {
+    unset($this->_sequences[$offset]);
+    $this->_sequences = array_values($this->_sequences);
+  }
+
+  /**
+  * Return the sequence list count.
+  *
+  * @see Countable::count()
+  * @return integer
+  */
+  public function count() {
+    return count($this->_sequences);
+  }
+
+  /**
+  * Return an iterator for the sequences
+  *
+  * @see IteratorAggregate::getIterator()
+  * @return Iterator
+  */
+  public function getIterator() {
+    return new ArrayIterator($this->_sequences);
+  }
+}
---
+++ tests/PhpCss/Ast/Sequence/ListTest.php
@@ -0,0 +1,126 @@
+<?php
+/**
+* Collection of tests for the Selector Sequence List class
+*
+* @license http://www.opensource.org/licenses/mit-license.php The MIT License
+* @copyright Copyright (c) 2010 Bastian Feder, Thomas Weinert
+*
+* @package PhpCss
+* @subpackage Tests
+*/
+
+/**
+* Load necessary files
+*/
+require_once(dirname(__FILE__).'/../../TestCase.php');
+
+/**
+* Test class for PhpCssAstSelectorSequenceList.
+*
+* @package PhpCss
+* @subpackage Tests
+*/
+class PhpCssAstSelectorSequenceTest extends PhpCssTestCase {
+
+  /**
+  * @covers PhpCssAstSelectorSequenceList::__construct
+  */
+  public function testConstructor() {
+    $this->assertInstanceOf(
+      'PhpCssAstSelectorSequenceList',
+      new PhpCssAstSelectorSequenceList()
+    );
+  }
+
+  /**
+  * @covers PhpCssAstSelectorSequenceList::__construct
+  * @covers PhpCssAstSelectorSequenceList::getIterator
+  */
+  public function testConstructorWithSequences() {
+    $sequences = array(
+      new PhpCssAstSelectorSequence()
+    );
+    $list = new PhpCssAstSelectorSequenceList(
+      $sequences
+    );
+    $this->assertSame(
+      $sequences, iterator_to_array($list)
+    );
+  }
+
+  /**
+  * @covers PhpCssAstSelectorSequenceList::__offsetExists
+  */
+  public function testOffsetExistsExpectingTrue() {
+    $list = new PhpCssAstSelectorSequenceList(
+      array(
+        new PhpCssAstSelectorSequence()
+      )
+    );
+    $this->assertTrue(isset($list[0]));
+  }
+
+  /**
+  * @covers PhpCssAstSelectorSequenceList::__offsetExists
+  */
+  public function testOffsetExistsExpectingFalse() {
+    $list = new PhpCssAstSelectorSequenceList();
+    $this->assertFalse(isset($list[0]));
+  }
+
+  /**
+  * @covers PhpCssAstSelectorSequenceList::__offsetGet
+  */
+  public function testOffsetGet() {
+    $list = new PhpCssAstSelectorSequenceList(
+      array(
+        $sequence = new PhpCssAstSelectorSequence()
+      )
+    );
+    $this->assertSame($sequence, $list[0]);
+  }
+
+  /**
+  * @covers PhpCssAstSelectorSequenceList::__offsetSet
+  */
+  public function testOffsetSetAppendsElement() {
+    $list = new PhpCssAstSelectorSequenceList();
+    $list[] = $sequence = new PhpCssAstSelectorSequence();
+    $this->assertSame($sequence, $list[0]);
+  }
+
+  /**
+  * @covers PhpCssAstSelectorSequenceList::__offsetSet
+  */
+  public function testOffsetSetReplacesElement() {
+    $list = new PhpCssAstSelectorSequenceList(
+      array(
+        $sequence = new PhpCssAstSelectorSequence()
+      )
+    );
+    $list[0] = $sequence = new PhpCssAstSelectorSequence();
+    $this->assertSame($sequence, $list[0]);
+  }
+
+  /**
+  * @covers PhpCssAstSelectorSequenceList::__offsetSet
+  */
+  public function testOffsetSetValidatesElementExpectingException() {
+    $list = new PhpCssAstSelectorSequenceList();
+    $this->setExpectedException('InvalidArgumentException');
+    $list[] = 'INVALID TYPE';
+  }
+
+  /**
+  * @covers PhpCssAstSelectorSequenceList::__offsetGet
+  */
+  public function testOffsetUnset() {
+    $list = new PhpCssAstSelectorSequenceList(
+      array(
+        new PhpCssAstSelectorSequence()
+      )
+    );
+    unset($list[0]);
+    $this->assertFalse(isset($list[0]));
+  }
+}
