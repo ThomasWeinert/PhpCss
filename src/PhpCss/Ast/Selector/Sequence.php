98b81caa9305f08468cbf4fe1cc80069751cad61
Thomas Weinert <thomas@weinert.info>
2012-06-07 15:49:13 +0200

    Adding a simple constructor to store arguments

--- src/PhpCss/Ast/Selector/Sequence.php
+++ src/PhpCss/Ast/Selector/Sequence.php
@@ -5,4 +5,8 @@ class PhpCssAstSelectorSequence extends PhpCssAstSelector {
   public $simples;
   public $combination;
 
+  public function __construct(array $simples = array(), PhpCssAstSelectorCombination $combination = NULL) {
+    $this->simples = $simples;
+    $this->combination = $combination;
+  }
 }
