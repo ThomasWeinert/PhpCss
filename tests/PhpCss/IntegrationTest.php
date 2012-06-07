2639831373bea705d26fd9644409c20fd9bdd1ca
Thomas Weinert <thomas@weinert.info>
2012-06-07 23:49:35 +0200

    Added and integration test that reformats css selectors

---
+++ tests/PhpCss/IntegrationTest.php
@@ -0,0 +1,50 @@
+<?php
+/**
+* Testing PhpCss usage
+*
+* @license http://www.opensource.org/licenses/mit-license.php The MIT License
+* @copyright Copyright 2012 PhpCss Team
+*
+* @package PhpCss
+* @subpackage Tests
+*/
+
+/**
+* Load necessary files
+*/
+require_once(dirname(__FILE__).'/TestCase.php');
+
+/**
+* Testing PhpCss usage
+*
+* @package PhpCss
+* @subpackage Tests
+*/
+class PhpCssIntegrationTest extends PhpCssTestCase {
+
+  /**
+  * @covers \stdClass
+  * @dataProvider provideReformattedCss
+  */
+  public function testReformatCss($expected, $selector) {
+    $scanner = new PhpCssScanner(new PhpCssScannerStatusSelector());
+    $tokens = array();
+    $scanner->scan($tokens, $selector);
+    $parser = new PhpCssParserDefault($tokens);
+    $ast = $parser->parse();
+    $visitor = new PhpCssAstVisitorCss();
+    $ast->accept($visitor);
+    $this->assertEquals(
+      $expected, (string)$visitor
+    );
+  }
+
+  public static function provideReformattedCss() {
+    return array(
+      array('*', '*'),
+      array('element', 'element'),
+      array('ns|*', 'ns|*'),
+      array('.class', '.class')
+    );
+  }
+}
